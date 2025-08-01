84a649d75005b30110cb95df8c4bd7171bf79375
devloper <developer@gmail.com>
2025-08-01 23:33:44 +0530

    commit by dev

---
+++ Spring-Security-Proj01-eclipse-git/src/main/java/com/nt/controller/OperationController.java
@@ -0,0 +1,13 @@
+package com.nt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OperationController {
+     
+	@GetMapping("/show")
+	public String home() {
+		return "welcome";
+	}
+}
