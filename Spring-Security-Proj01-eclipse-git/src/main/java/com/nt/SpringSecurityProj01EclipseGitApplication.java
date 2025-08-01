cecf3e750c6ca3fafcb22dd1dfe65998c9520e40
tl <tl@gmail.com>
2025-08-01 17:26:23 +0530

    initial commit

---
+++ Spring-Security-Proj01-eclipse-git/src/main/java/com/nt/SpringSecurityProj01EclipseGitApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProj01EclipseGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProj01EclipseGitApplication.class, args);
+	}
+
+}
